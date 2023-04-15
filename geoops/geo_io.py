c478dc2df56d2e83a27fe778f7c3f5edd6c34279
Gary <garynobles20@gmail.com>
2023-04-15 12:30:49 +0100

    def to_arcgis disabled

--- geoops/geo_io.py
+++ geoops/geo_io.py
@@ -87,42 +87,42 @@ def read_files(file_path, rows_per_request=0, offset=0, crs=27700):
 
 
 
-def to_arcgis_geodb(data, gdb_path, name, schema=None, overwrite=True):
-    if pkgutil.find_loader("arcpy") is not None:
-
-        # Create a temporary directory and get its name
-        with tempfile.TemporaryDirectory() as tmpdir:
-            # Create a GeoDataFrame with some data
-            data = gpd.GeoDataFrame({"id": [1, 2], "geometry": [Point(0, 0), Point(1, 1)]})
-
-            # Write the GeoDataFrame to a temporary file in the temporary directory
-            tmpfile = tmpdir + "/data.geojson"
-            data.to_file(tmpfile, driver="GeoJSON")
-
-            # Read the temporary file back into a new GeoDataFrame
-            # new_data = gpd.read_file(tmpfile)
-
-            # Do something with the new GeoDataFrame
-            # print(new_data.head())
-
-        # Check if the geometry type of the GeoDataFrame is 'Polygon'
-        is_polygon = data.geometry.geom_type.isin(['Polygon']).all()
-
-        if is_polygon:
-            print('The GeoDataFrame contains only Polygon geometries.')
-            geom_type = 'Polygon'
-        else:
-            print('The GeoDataFrame contains at least one non-Polygon geometry.')
-
-        if schema is not None:
-            pass
-            # arcpy.conversion.JSONToFeatures(
-            #     in_json_file=tmpfile,
-            #     out_file=os.path.join(gdb_path, schema, name),
-            #     geometry_type=geom_type
-            # )
-    else:
-        print('arcpy not found, unable to write')
+# def to_arcgis_geodb(data, gdb_path, name, schema=None, overwrite=True):
+#     if pkgutil.find_loader("arcpy") is not None:
+#
+#         # Create a temporary directory and get its name
+#         with tempfile.TemporaryDirectory() as tmpdir:
+#             # Create a GeoDataFrame with some data
+#             data = gpd.GeoDataFrame({"id": [1, 2], "geometry": [Point(0, 0), Point(1, 1)]})
+#
+#             # Write the GeoDataFrame to a temporary file in the temporary directory
+#             tmpfile = tmpdir + "/data.geojson"
+#             data.to_file(tmpfile, driver="GeoJSON")
+#
+#             # Read the temporary file back into a new GeoDataFrame
+#             # new_data = gpd.read_file(tmpfile)
+#
+#             # Do something with the new GeoDataFrame
+#             # print(new_data.head())
+#
+#         # Check if the geometry type of the GeoDataFrame is 'Polygon'
+#         is_polygon = data.geometry.geom_type.isin(['Polygon']).all()
+#
+#         if is_polygon:
+#             print('The GeoDataFrame contains only Polygon geometries.')
+#             geom_type = 'Polygon'
+#         else:
+#             print('The GeoDataFrame contains at least one non-Polygon geometry.')
+#
+#         if schema is not None:
+#             pass
+#             # arcpy.conversion.JSONToFeatures(
+#             #     in_json_file=tmpfile,
+#             #     out_file=os.path.join(gdb_path, schema, name),
+#             #     geometry_type=geom_type
+#             # )
+#     else:
+#         print('arcpy not found, unable to write')
 
 # url = "https://services-eu1.arcgis.com/ZOdPfBS3aqqDYPUQ/arcgis/rest/services/National_Heritage_List_for_England_NHLE/FeatureServer/3/query?outFields=*&where=1%3D1&f=geojson"
 # read_files(url, rows_per_request=2000)
