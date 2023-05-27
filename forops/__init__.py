ccb3a038f25af817ff6ac0b6864f195a2856f1ef
Gary <garynobles20@gmail.com>
2023-05-27 23:13:17 +0100

    New version check and wheel updated to 0.0.6

--- forops/__init__.py
+++ forops/__init__.py
@@ -4,13 +4,17 @@ from .stats import *
 import subprocess
 import warnings
 
+import subprocess
+import warnings
+
 
 def check_newer_version(package_name, current_version):
     try:
         output = subprocess.check_output(['pip', 'list', '--outdated'])
         packages = output.decode().strip().split('\n')[2:]
         for package in packages:
-            name, version, _ = package.split()
+            name, *other_info = package.split()
+            version = other_info[0]
             if name.lower() == package_name.lower() and version > current_version:
                 return True
     except subprocess.CalledProcessError:
@@ -19,12 +23,17 @@ def check_newer_version(package_name, current_version):
     return False
 
 
-package_name = 'forops'
-current_version = '0.0.5'
+def _warn_if_newer_version_available():
+    package_name = 'forops'
+    current_version = '0.0.6'
 
-if check_newer_version(package_name, current_version):
-    warning_message = f"A newer version of {package_name} is available. Please consider upgrading."
-    warnings.warn(warning_message, UserWarning)
-else:
-    warning_message = f"This is the current version of {package_name}, beware it is in development."
-    warnings.warn(warning_message, UserWarning)
+    if check_newer_version(package_name, current_version):
+        warning_message = f"A newer version of {package_name} is available. Please consider upgrading."
+        warnings.warn(warning_message, UserWarning)
+    else:
+        warning_message = f"This is the latest version of {package_name}. Beware it is under development, please report any bugs."
+        warnings.warn(warning_message, UserWarning)
+
+
+
+_warn_if_newer_version_available()
